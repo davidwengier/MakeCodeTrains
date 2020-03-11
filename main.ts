c7826e1492eafea4d9acdd74ee4625dd4809ce89
David Wengier <david.wengier@microsoft.com>
2020-03-11 12:23:04 +1100

    Make it nicer, and look like a train game

--- main.ts
+++ main.ts
@@ -1,51 +1,52 @@
 namespace SpriteKind {
     export const Track = SpriteKind.create()
 }
+function updateCursor () {
+    cursor.setPosition(x, y)
+}
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    y = 16 - y
-    updatePointer()
+    x += -16
+    updateCursor()
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    x = 16 + x
-    updatePointer()
+    y = 16 + y
+    updateCursor()
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite = sprites.create(img`
+    trackPiece = sprites.create(img`
 . . . . . . . . . . . . . . . . 
-. . . . 6 6 6 6 6 6 6 6 . . . . 
-. . . 6 9 6 6 6 6 6 6 c 6 . . . 
-. . 6 c 9 6 6 6 6 6 6 c c 6 . . 
-. 6 c c 9 9 9 9 9 9 6 c c 9 6 d 
-. 6 c 6 8 8 8 8 8 8 8 b c 9 6 6 
-. 6 6 8 b b 8 b b b 8 8 b 9 6 6 
-. 6 8 b b b 8 b b b b 8 6 6 6 6 
-. 8 8 6 6 6 8 6 6 6 6 6 8 6 6 6 
-. 8 8 8 8 8 8 f 8 8 8 f 8 6 d d 
-. 8 8 8 8 8 8 f 8 8 f 8 8 8 6 d 
-. 8 8 8 8 8 8 f f f 8 8 8 8 8 8 
-. 8 f f f f 8 8 8 8 f f f 8 8 8 
-. . f f f f f 8 8 f f f f f 8 . 
-. . . f f f . . . . f f f f . . 
+. . . . . . . . . . . . . . . . 
+. . f f . . . f f . . . f f . . 
+f f f f f f f f f f f f f f f f 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+f f f f f f f f f f f f f f f f 
+. . f f . . . f f . . . f f . . 
+. . f f . . . f f . . . f f . . 
+. . f f . . . f f . . . f f . . 
+. . f f . . . f f . . . f f . . 
+f f f f f f f f f f f f f f f f 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+f f f f f f f f f f f f f f f f 
+. . f f . . . f f . . . f f . . 
+. . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, SpriteKind.Track)
-    mySprite.setPosition(y, x)
+    trackPiece.setPosition(x, y)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    y = 16 + y
-    updatePointer()
+    x = 16 + x
+    updateCursor()
 })
-function updatePointer () {
-    mySprite2.setPosition(y, x)
-}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    x += -16
-    updatePointer()
+    y += -16
+    updateCursor()
 })
-let mySprite: Sprite = null
-let x = 0
+let trackPiece: Sprite = null
 let y = 0
-let mySprite2: Sprite = null
-mySprite2 = sprites.create(img`
+let x = 0
+let cursor: Sprite = null
+scene.setBackgroundColor(1)
+cursor = sprites.create(img`
 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
 5 . . . . . . . . . . . . . . 5 
 5 . . . . . . . . . . . . . . 5 
@@ -63,4 +64,6 @@ mySprite2 = sprites.create(img`
 5 . . . . . . . . . . . . . . 5 
 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
 `, SpriteKind.Player)
-updatePointer()
+x = 8
+y = 8
+updateCursor()
